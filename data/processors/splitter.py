c35a855ff9a713fbf5e1fcce6654f222d03f9854
DanielWestgaard <daniel.westgaard@gmail.com>
2025-04-01 22:17:48 +0200

    comment

--- data/processors/splitter.py
+++ data/processors/splitter.py
@@ -1,3 +1,5 @@
+# IMPLEMENTED IN time_series_ml.py
+
 class TimeSeriesSplitter:
     """Handles time series-specific train/validation/test splitting"""
     def train_test_split(self, data, test_size=0.2, validation_size=0.2):
