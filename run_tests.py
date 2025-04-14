3bd182b2916244834ef06e74b63352c2ff35a680
DanielWestgaard <daniel.westgaard@gmail.com>
2025-04-14 11:54:59 +0200

    remvoed unused file and added a possible jupyter notebook for personal testing

--- run_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import tests.data.test_cleaner as test_cleaner
-
-def main():
-    test_cleaner.TestDataCleaner("subTest").test_initialization()
-
-if __name__ == "__main__":
-    exit(main())
