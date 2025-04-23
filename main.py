482103107645772649f2d7ecdc77a1b4d1778bb4
DanielWestgaard <72098952+DanielWestgaard@users.noreply.github.com>
2025-04-23 19:14:00 +0200

    tested pipeline run, and still works well!

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Train hybrid trading strategy models with uncertainty quantification')
     
     parser.add_argument('--broker-func', action='store_true', default=False, help='Test all broker functionality')
-    parser.add_argument('--data-pipeline', action='store_true', default=False, help='Run data processing pipeline')
+    parser.add_argument('--data-pipeline', action='store_true', default=True, help='Run data processing pipeline')
     parser.add_argument('--walk-forward-analysis', action='store_true', default=False, 
                         help='Run Walk-Forward Testing and Cross-Validation Implementation with Backtesting system.')
-    parser.add_argument('--train-model', action='store_true', default=True, help='Train model')
+    parser.add_argument('--train-model', action='store_true', default=False, help='Train model')
     parser.add_argument('--backtest-trained-model', action='store_true', default=False, help='Run backtesting on trained model')
     parser.add_argument('--test-backtest', action='store_true', default=False, help='Test backtesting (simple)')
     parser.add_argument('--live-integration', action='store_true', default=False, help='Test backtesting (simple)')
@@ -35,7 +35,7 @@ def parse_arguments():
 def main():
     args = parse_arguments()
     log_utils._is_configured = False
-    logger = log_utils.setup_logging(name="live", log_to_file=False, log_level=sys_config.DEBUG_LOG_LEVEL)
+    logger = log_utils.setup_logging(name="data", log_to_file=True, log_level=sys_config.DEBUG_LOG_LEVEL)
     
     if args.broker_func:
         broker = CapitalCom()
